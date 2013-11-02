a4e9185860850580ba6473965fa26993c3fa395c
Veselin Todorov <veselin@veselin.bg>
2013-11-03 00:34:01 +0200

    Add test for standard comparison

---
+++ test/cases/comparison/standard.js
@@ -0,0 +1,6 @@
+/* stylec comparison: standard */
+
+var good = 1 == 3;
+var good = 1 != 3;
+var bad = 1 === 3;
+var bad = 1 !== 3;
---
+++ test/cases/comparison/standard.result.js
@@ -0,0 +1,4 @@
+module.exports = [
+  { line: 5, column: 10 },
+  { line: 6, column: 10 },
+];
