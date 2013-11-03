6c0f6cbb903f5e8feb678f04c0635a160cb29847
Veselin Todorov <veselin@veselin.bg>
2013-11-03 03:45:40 +0200

    Add spaces after unary operator rule

---
+++ test/cases/unary/test.js
@@ -0,0 +1,7 @@
+/* stylec unary: 1 */
+
+var bad = !good;
+var bad = !!good;
+
+var good = ! ! good;
+var good = ! good;
---
+++ test/cases/unary/test.result.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { line: 3, column: 10 },
+  { line: 4, column: 10 },
+  { line: 4, column: 11 },
+];
